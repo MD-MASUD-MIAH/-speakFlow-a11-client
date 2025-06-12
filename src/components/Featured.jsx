a42e8084d119929b6b13cc6b4d8353b49993852f
176409 <148116817+176409@users.noreply.github.com>
2025-06-13 00:00:54 +0600

    tanStack table

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,21 +1,89 @@
-import React from 'react';
-import { useLoaderData } from 'react-router';
 
+import { useLoaderData } from 'react-router';
+import { useReactTable,getCoreRowModel,flexRender } from '@tanstack/react-table';
+import { Link } from 'react-router';
 const Featured = () => {
 
     const topTen = useLoaderData() 
 
-    console.log(topTen);
+    console.log(topTen); 
+    const columns =[
+      {
+        header:'No',
+        accessorKey:'index',
+        cell:(info)=><p>{info.row.index + 1}</p>
+
+      },
+      {
+        header:'Image',
+        accessorKey:'image',
+          cell: info => (
+    <>
+    
+      <div className="flex items-center gap-3">
+            <div className="avatar">
+              <div className="mask mask-squircle h-12 w-12">
+                 <img
+        src={info.getValue()}
+        alt="Thumbnail"
+        className=""
+      />
+              </div>
+            </div>
+    </div>
+    </>
+    ),
+      },
+
+       {
+        header:'Title',
+        accessorKey:'title' ,
+
+      },
+      {
+        header:'Description',
+        accessorKey:'longDesc',
+        cell:info=><div><p className='max-w-full md:max-w-[400px] overflow-hidden text-ellipsis whitespace-nowrap'>{info.getValue().slice(0,200)}</p> </div>
+      },
+       {
+        header:'Details',
+        accessorKey:'_id',
+        cell:info=><div>  <Link to={`/details/${info.getValue()}`} className="btn tom-btn">
+              Details
+            </Link> </div>
+      }
+    ]
+
+  
+const table = useReactTable({data:topTen,columns,getCoreRowModel:getCoreRowModel()})
     
     return (
-        <div>
-            <h1> Featured Blogs
-</h1>
+        <div className='w-11/12 mx-auto overflow-x-auto'>
+             <div>
+                  <h1 className="text-center font-bold text-xl md:text-4xl pt-10 uppercase ">
+      Top 10 Blogs
+      </h1>
+      <p className="text-center pt-4 pb-10  text-[#7f8c8d]">
+    Discover some of the world’s most inspiring and influential blogs. From tech trends and personal growth  
+        <br /> <span className='hidden md:block'> to movies and money and creative ideas to explore and enjoy.</span>
+      </p>
+             </div>
 
-{
 
-    topTen.map((res,index)=><p key={res._id} res={res}> {index+1} {res.longDesc}</p>)
-}
+
+
+
+<table className='table'> 
+  {table.getHeaderGroups().map((headerGroup,index)=><tr key={index}>{headerGroup.headers.map((header,index)=><th key={index}>{flexRender(header.column.columnDef.header,header.getContext())}</th>)}</tr>)}
+
+  <thead>
+   
+  </thead> 
+  <tbody>
+   {table.getRowModel().rows.map((row,index)=><tr key={index}>{row.getVisibleCells().map((cell,index)=><td key={index}>{flexRender(cell.column.columnDef.cell,cell.getContext())}</td>)}</tr>)}
+  </tbody>
+</table>
+
         </div>
     );
 };
